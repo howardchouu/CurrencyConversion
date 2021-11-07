eb4ce67fbdea704921634c50c557908974279fbc
howardchouu <87143391+howardchouu@users.noreply.github.com>
2021-11-07 23:20:45 +0800

    automatic work for create table

---
+++ conf/demo.sql
@@ -0,0 +1,9 @@
+TABLE CREATE calculate_record (
+    currency varchar(10),
+    rate numeric(16,2),
+    price numeric(16,2),
+    discount numeric(16,2),
+    result numeric(16,2),
+    record_time timestamp with time zone DEFAULT ('now'::text)::timestamp
+with time zone
+);
